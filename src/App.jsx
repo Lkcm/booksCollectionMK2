38a16594ff05f7a6622805603c456caf3b6b3897
Lucas Kroger <lucasmkroger12@gmail.com>
2023-02-09 16:29:44 -0300

    criacao do projeto book collection 2.0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-3xl">Hello World (Final Test)</div>
+  )
+}
+
+export default App
